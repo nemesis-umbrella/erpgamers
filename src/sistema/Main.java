c04203f0ceadde2c810f6b85095ebabf51b7c3b5
Jorge L. Mondragón <nemesis_umbrella@outlook.com>
2018-02-24 19:52:08 -0600

    Contiene cambios del main

---
+++ src/sistema/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Creado por Jorge L. Mondragón
+ * Versión 1.0
+ * Creado el 24/02/2018
+ */
+package sistema;
+
+/**
+ *
+ * @author nemesis
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }
+}
