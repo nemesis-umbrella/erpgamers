eb0dcbfdb8ad2cae61528100654fbc54a608c87f
Jorge L. Mondragón <nemesis_umbrella@outlook.com>
2018-02-24 20:57:32 -0600

    Se ha definido MenuPrincipal como el JFrame principal del sistema

--- src/sistema/Main.java
+++ src/sistema/Main.java
@@ -4,13 +4,16 @@
  * Creado el 24/02/2018
  */
 package sistema;
-
+import menuprincipal.MenuPrincipal;
 /**
  *
  * @author nemesis
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hola mundo");
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setVisible(true);
+        menu.setResizable(false);
+        menu.setLocationRelativeTo(null);
     }
 }
