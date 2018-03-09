7a666198fcc1a58eac3cf6b58877985a23e47238
Jorge L. Mondragón <nemesis_umbrella@outlook.com>
2018-03-08 18:03:50 -0600

    Se ha configurado el color base del sistema

---
+++ src/recursos/Colores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursos;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Jorge L. Mondragón <nemesis_umbrella@outlook.com>
+ */
+public class Colores {
+    public static Color colorAzul(){
+        Color color = new Color(0, 71, 179);
+        return color;
+    }
+    public static Color colorBlanco(){
+        Color color = new Color(255, 255, 255);
+        return color;
+    }
+}
