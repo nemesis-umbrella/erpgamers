7679a4ed2ca34f5eee229e2c6caaead8669173aa
Jorge L. Mondragón <nemesis_umbrella@outlook.com>
2018-03-08 18:04:06 -0600

    Se han configurado las fuentes para el sistema

---
+++ src/recursos/Fuente.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursos;
+
+import java.awt.Font;
+
+/**
+ *
+ * @author Jorge L. Mondragón <nemesis_umbrella@outlook.com>
+ */
+public class Fuente {
+    public static Font fuenteTitulos(){
+        Font fuente = new Font("Andale Mono", 3, 18);
+        return fuente;
+    }
+    
+    public static Font fuenteTexto12(){
+        Font fuente = new Font("Andale Mono", 1, 12);
+        return fuente;
+    }
+    
+    public static Font fuenteTexto14(){
+        Font fuente = new Font("Andale Mono", 1, 14);
+        return fuente;
+    }
+}
