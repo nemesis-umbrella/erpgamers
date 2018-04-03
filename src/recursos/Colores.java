22b8dd0d8bb1b36f427e566bd09754121c809250
Jorge L. Mondragón <nemesis_umbrella@outlook.com>
2018-04-02 19:57:35 -0500

    Se ha configurado el color por seleccionado por el usuario

--- src/recursos/Colores.java
+++ src/recursos/Colores.java
@@ -5,6 +5,9 @@
  */
 package recursos;
 
+import conexion.Conexion;
+import iniciosesion.InicioSesionOperaciones;
+import iniciosesion.Perfil;
 import java.awt.Color;
 
 /**
@@ -12,6 +15,38 @@ import java.awt.Color;
  * @author Jorge L. Mondragón <nemesis_umbrella@outlook.com>
  */
 public class Colores {
+    private static int seleccion = 0;
+
+    public static int getSeleccion() {
+        return seleccion;
+    }
+
+    public static void setSeleccion(int seleccion) {
+        Colores.seleccion = seleccion;
+    }
+    
+    public static Color cargarColor(){
+        Color color;
+        switch(seleccion){
+            case 0:
+                color = new Color(0, 71, 179);
+                break;
+            case 1: 
+                color = new Color(0,0,0);
+                break;
+            case 2:
+                color = new Color(178,0,44);
+                break;
+            case 3:
+                color = new Color(80,0,255);
+                break;
+            default:
+                color = new Color(0, 71, 179);
+                break;
+        } 
+        return color;
+    }
+    
     public static Color colorAzul(){
         return new Color(0, 71, 179);
     }
@@ -29,5 +64,17 @@ public class Colores {
     
     public static Color colorMorado(){
         return new Color(80,0,255);
+    }
+    
+    public static String guardarColorNuevo(int sel){
+        String error = "";
+        InicioSesionOperaciones iniop = new InicioSesionOperaciones();
+        try {
+            Colores.setSeleccion(sel);
+            iniop.guardarColor(Conexion.obtener(), Perfil.getLogin(), sel);
+        } catch (Exception e) {
+            error = "Error: "+e;
+        }
+        return error;
     }
 }
