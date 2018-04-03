4b1a9685b90e4952d9d61b7aed19aaeaa4b2a158
Alejandra Lopez <moon180192@hotmail.com>
2018-04-03 14:41:43 -0500

    botones y texto

--- src/contabilidad/Facturas.java
+++ src/contabilidad/Facturas.java
@@ -17,7 +17,7 @@ public class Facturas extends javax.swing.JFrame {
     public Facturas() { 
         initComponents();
     }
-
+ 
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -36,24 +36,23 @@ public class Facturas extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
+        matemp = new javax.swing.JTextField();
+        idcliente = new javax.swing.JTextField();
+        nombre = new javax.swing.JTextField();
+        direccion = new javax.swing.JTextField();
         jRadioButton1 = new javax.swing.JRadioButton();
         jRadioButton2 = new javax.swing.JRadioButton();
         jRadioButton3 = new javax.swing.JRadioButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jTextField5 = new javax.swing.JTextField();
-        jTextField6 = new javax.swing.JTextField();
+        agregar = new javax.swing.JButton();
+        btnMostrar = new javax.swing.JButton();
+        btnSalir = new javax.swing.JButton();
+        idprov = new javax.swing.JTextField();
+        cantidad = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable2 = new javax.swing.JTable();
+        tablalistado = new javax.swing.JTable();
+        jLabelCopyright = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -91,13 +90,10 @@ public class Facturas extends javax.swing.JFrame {
 
         jLabel7.setText("Cantidad");
         getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 210, -1, -1));
-
-        jButton1.setText("Agregar");
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 210, -1, -1));
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, 100, -1));
-        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 20, 100, -1));
-        getContentPane().add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 80, 120, -1));
-        getContentPane().add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 80, 210, -1));
+        getContentPane().add(matemp, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, 100, -1));
+        getContentPane().add(idcliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 20, 100, -1));
+        getContentPane().add(nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 80, 120, -1));
+        getContentPane().add(direccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 80, 210, -1));
 
         jRadioButton1.setText("No.Resp.Insc");
         getContentPane().add(jRadioButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 140, -1, -1));
@@ -108,18 +104,29 @@ public class Facturas extends javax.swing.JFrame {
         jRadioButton3.setText("Consumidor Final");
         getContentPane().add(jRadioButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 140, -1, -1));
 
-        jButton2.setText("Aceptar");
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 470, -1, 20));
+        agregar.setBackground(new java.awt.Color(0, 177, 50));
+        agregar.setForeground(new java.awt.Color(253, 251, 251));
+        agregar.setText("Agregar");
+        getContentPane().add(agregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 210, -1, 20));
 
-        jButton3.setText("Cancelar");
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 470, -1, 20));
+        btnMostrar.setBackground(new java.awt.Color(0, 177, 50));
+        btnMostrar.setForeground(new java.awt.Color(253, 251, 251));
+        btnMostrar.setText("Aceptar");
+        btnMostrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMostrarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnMostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 430, -1, 20));
 
-        jButton4.setText("Salir");
-        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 470, 70, 20));
-        getContentPane().add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 210, 80, -1));
-        getContentPane().add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 210, 50, -1));
+        btnSalir.setBackground(new java.awt.Color(233, 1, 48));
+        btnSalir.setForeground(new java.awt.Color(253, 251, 251));
+        btnSalir.setText("Cancelar");
+        getContentPane().add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 430, 80, 20));
+        getContentPane().add(idprov, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 210, 80, -1));
+        getContentPane().add(cantidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 210, 50, -1));
 
-        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+        tablalistado.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -130,19 +137,23 @@ public class Facturas extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane2.setViewportView(jTable2);
+        jScrollPane2.setViewportView(tablalistado);
 
         getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 270, 480, 130));
 
-        jMenuBar1.add(jMenu1);
+        jLabelCopyright.setText("©2018 Gamers Retail ");
+        getContentPane().add(jLabelCopyright, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 500, -1, -1));
 
-        jMenu2.setText("Regresar");
-        jMenuBar1.add(jMenu2);
+        jMenuBar1.add(jMenu1);
 
         setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnMostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMostrarActionPerformed
+        tablalistado.setModel( mostrar());
+    }//GEN-LAST:event_btnMostrarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -180,10 +191,13 @@ public class Facturas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton agregar;
+    private javax.swing.JButton btnMostrar;
+    private javax.swing.JButton btnSalir;
+    private javax.swing.JTextField cantidad;
+    private javax.swing.JTextField direccion;
+    private javax.swing.JTextField idcliente;
+    private javax.swing.JTextField idprov;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -191,8 +205,8 @@ public class Facturas extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabelCopyright;
     private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JRadioButton jRadioButton1;
     private javax.swing.JRadioButton jRadioButton2;
@@ -200,12 +214,8 @@ public class Facturas extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTable jTable2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField matemp;
+    private javax.swing.JTextField nombre;
+    private javax.swing.JTable tablalistado;
     // End of variables declaration//GEN-END:variables
 }
